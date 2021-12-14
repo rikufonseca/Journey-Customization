b31d826d53a94f54d167d87d735bc3e1ecae07cb
Rika Fonseca <fonsecarika@gmail.com>
2021-12-14 12:43:24 +0200

    basicrouting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :journeys, only: [:index, :show ] do
+    resources :bookings, only: [:new, :create, :edit, :update]
+
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :journey, only: [:destroy, :new, :create, :edit, :update, :accept]
+resources :bookings, only: [:destroy]
 end
