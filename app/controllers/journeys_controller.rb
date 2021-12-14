c80d4a15f79163d753c98f4daa24a186a96052fd
Sarah Khoubbaz-harl <s-kh@live.fr>
2021-12-14 16:58:49 +0100

    controller eud done + view/form for edit

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,5 +1,6 @@
 class JourneysController < ApplicationController
-  
+  before_action :set_journey, only: %i[show edit update destroy]
+
   def index
   end
 
@@ -11,4 +12,28 @@ class JourneysController < ApplicationController
 
   def create
   end
+
+  # GET /journeys/:id/edit
+  def edit
+  end
+
+  # PATCH /journeys/:id
+  def update
+    if @journey.update(garden_params)
+      redirect_to journeys_path, notice: 'Garden was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @journey.destroy
+    redirect_to journeys_path, notice: 'Journey was successfully destroyed.'
+  end
+end
+
+private
+
+def set_journey
+  @journey = Journey.find(params[:id])
 end
