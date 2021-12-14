427bd4e1c0d9114830f0e544f4a8552d48c473a4
Rika Fonseca <fonsecarika@gmail.com>
2021-12-14 17:06:18 +0200

    controller-journey creer et view

---
+++ test/controllers/journeys_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class JourneysControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
