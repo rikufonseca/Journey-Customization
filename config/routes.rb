631a6fb576f7b483c14cc0f18ad9b6b903115967
Rika Fonseca <fonsecarika@gmail.com>
2021-12-14 13:02:48 +0200

    last changement

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :journeys, only: [:index, :show ] do
-    resources :bookings, only: [:new, :create, :edit, :update]
-
-end
+  resources :journeys do
+    resources :bookings, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :journey, only: [:destroy, :new, :create, :edit, :update, :accept]
-resources :bookings, only: [:destroy]
+  resources :bookings, only: [:destroy, :edit, :update]  do 
+    member do 
+      get :accept
+      get :refuse
+    end
+  end
 end
