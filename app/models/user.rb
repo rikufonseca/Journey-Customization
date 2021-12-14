a2391aa75f3c394ec3750d006e7eca1e48f59c8c
Rika Fonseca <fonsecarika@gmail.com>
2021-12-14 15:22:19 +0200

    model booking with hasmanys add for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :bookings
+  has_many :journeys
   validates :full_name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true 
   end 
---
+++ db/migrate/20211214131631_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookings do |t|
+      t.string :status
+      t.integer :start_date
+      t.integer :number_of_people
+      t.references :journey, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
