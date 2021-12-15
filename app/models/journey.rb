302ec4ba57a5ba58ef4838672f30e8cd88e8001c
Rika Fonseca <fonsecarika@gmail.com>
2021-12-15 13:50:42 +0200

    advanced seeding

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,4 +1,6 @@
 class Journey < ApplicationRecord
+  SENSES = ['Best Sight', "Amazing Hearings", 'Best Tastes', "Amazing Touch", 'Best Smell']
+  ENVIRONMENTS = ["Cloudy", "Sunny", "Windy", "calm", "loudy"]
   validates :title, uniqueness: true, length: { minimum: 6 }, presence: true
   validates :address, presence: true, length: { minimum: 10 }
   validates :duration, presence: true
