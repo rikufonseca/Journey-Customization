7b94f232fa4577f41b88547a1a047fb8b0eb2d9e
Rika Fonseca <fonsecarika@gmail.com>
2021-12-15 17:55:27 +0200

    problem avec new dans les params de journey id

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,35 @@
+class BookingsController < ApplicationController
+
+  def new
+  # @user = User.find(params[:user_id])
+   @journey = Journey.find(params[:journey_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @journey = Journey.find(params[:booking][:journey_id])
+    @booking = Booking.new(params_booking)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def accept
+  end
+
+  def refuse
+  end
+
+  private 
+
+  def params_booking
+    params.require(:booking).permit(:start_date, :number_of_people)
+  end
+end
