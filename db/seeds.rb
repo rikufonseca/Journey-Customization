7fbb959b5f0031202e25da926b63d9e4365f824f
Sarah Khoubbaz-harl <s-kh@live.fr>
2021-12-16 21:53:36 +0100

    could understand where the problem came from but not resolve it

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'csv'
 require 'faker'
 
@@ -34,7 +26,7 @@ CSV.foreach(Rails.root.join('lib/lieux_culturels.csv'), csv_options) do |row|
   address = row["Adresse 1"] + " " + row["Code Postal"] + " " + row["Ville"]
 
   journey = Journey.create(title: title,
-                  interest: Journey::INTERESTS.sample(rand(1..5)),
+                  interest: Journey::INTERESTS.sample(rand(1..7)),
                   address: address,
                   user_id: User.all.sample.id,
                   duration: rand(30..300),
@@ -49,6 +41,7 @@ CSV.foreach(Rails.root.join('lib/lieux_culturels.csv'), csv_options) do |row|
   journey.save
 
  puts 'created 1 journey .....'
+ puts journey.interest.join(', ')
  end
 
  puts ' done seeding ......'
